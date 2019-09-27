7f34b218426e53f09cf5f7c2f0ceedc7627f8a8e
Caio Costa <caiocostaee@gmail.com>
2019-09-26 21:37:36 -0300

    adding route bia

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -35,6 +35,16 @@ router.get('/list', async (req, res) => {
     }
 });
 
+router.get('/bia', async (req, res) => {
+    res.send(
+        <div>
+            <h1>Clique no botão</h1>
+            <button onClick={() => alert('bia feia (:')}>CLIQUE AQUI</button>
+        
+        </div>
+    )
+});
+
 module.exports = app => {
     return app.use('/auth', router);
 }
